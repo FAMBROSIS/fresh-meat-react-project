a9df5e0cd702d215ab3092f83674afe1dd841aa9
Deron <fambroderon@gmail.com>
2023-10-11 14:26:33 -0400

    branch switch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,29 @@
 import React ,{createContext, useState} from 'react'
+import HomePage from './HomePage'
+import TimeWrapper from './TimeWrapper';
+import Content from './Content';
+import Converter from './Converter';
+import Tools from './Tools';
 
+// let timer = useContext(TimerContext);
 
-export const NavContext = createContext();
+// export const TimerContext = createContext();
 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <Link to='/homepage'>Home</Link>
+        <Link to='/content'>While you wait</Link>
+        <Link to='/tools'>Tools</Link>
+
+        <Routes>
+          <Route path="/homepage" element={<HomePage />} />
+          <Route path="/content" element={<Content />} />
+          <Route path="/tools" element={<Tools />} />
+        </Routes>
+
+    </div>
   )
 }
 
