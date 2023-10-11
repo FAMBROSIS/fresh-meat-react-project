f01f9ca628bd776f7a1f2693b6a2d955c3819a1e
Deron <fambroderon@gmail.com>
2023-10-11 14:27:26 -0400

    branch switch

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Tools = () => {
   return (
-    <div>Tools</div>
-  )
-}
+    <div>
+      <div id="tools">
+        <Converter />
+      </div>
+    </div>
+  );
+};
 
-export default Tools
+export default Tools;
